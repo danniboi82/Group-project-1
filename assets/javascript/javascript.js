df77b54b40b1c40b34cecf8dacfa502531bb98b1
Unknown <danoh82@gmail.com>
2017-11-08 21:13:57 -0800

    Created rough draft for web page
    ROUGH DRAFT

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+//API KEY FOR MEETUP : 2c1417672b6d787310406c51316d528
+//API KEY FOR iEVENT : mfrnWNrHSVck5Cqk
+
+$.ajax({
+    url:"https://api.meetup.com/2/events?key=2c1417672b6d787310406c51316d528&group_urlname=ny-tech&sign=true#meta",
+    method: GET,
+}) .done(function(){
+
+});
