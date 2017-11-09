c4ac5517d09c5ca1003001f4a411fbe28e5dd786
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-09 14:24:59 -0800

    Merge branch 'Katrina'
    added assets folder, added links for jQuery and Bootstrap, created object got default pics

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,14 @@
-var Image = {
-    band:
-    concert:
-    sports:
-    theater_broadway_national_tours:
-    auto_racing:
-}
+
+$(document).ready(function(){
+
+//  Object to keep default images if IPA does not provide them
+    var Image = {
+        band: "assest/images/band.jpg",
+        concert: "assest/images/concert.jpg",
+        sports: "assest/images/sport.jpg",
+        theater_broadway_national_tours: "assest/images/theater.jpg",
+        auto_racing: "assest/images/auto_racing.jpg"
+    };
+
+    console.log(Image.band);
+});
