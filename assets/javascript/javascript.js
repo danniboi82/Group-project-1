38d38ee4b0530337403a1e3bbb891fcc02fbe16a
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-11-12 13:40:45 -0800

    added function that found a picture for a event
    the function takes 1 event as a parameter and return an array of pics for the event. By default we can use only first elememt of the array  pic[0]

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,54 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-//  Object to keep default images if IPA does not provide them
+    //  Object to keep default images if IPA does not provide them
     var Image = {
         band: "assest/images/band.jpg",
         concert: "assest/images/concert.jpg",
         sports: "assest/images/sport.jpg",
         theater_broadway_national_tours: "assest/images/theater.jpg",
-        auto_racing: "assest/images/auto_racing.jpg"
+        auto_racing: "assest/images/auto_racing.jpg",
+        theater: "assest/images/theater_2.jpg",
+        default: "assest/images/default.jpg"
     };
 
     console.log(Image.band);
 
 
-// get  current position from browser, return Latitude and Longitude
-var browserLatitude, browserLongitude;
+    // get  current position from browser, return Latitude and Longitude
+    var browserLatitude, browserLongitude;
 
-(function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
-            browserLatitude = position.coords.latitude;
-            browserLongitude = position.coords.longitude; 
-       
-        });
-    } else { 
-        // for testing
-        console.log("Geolocation is not supported by this browser.");
+    (function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                browserLatitude = position.coords.latitude;
+                browserLongitude = position.coords.longitude;
+
+            });
+        } else {
+            // for testing
+            console.log("Geolocation is not supported by this browser.");
+        }
+    })();
+
+    // function takes 1 event (response.events[i]) from seatgeek ajax response
+    function getPic(seatgeekEvent) {
+        var pic = [];
+        // if few performers for the event --> make an array of pics.
+        for (var i = 0; i < seatgeekEvent.performers.length; i++) {
+            var image = seatgeekEvent.performers[i].image;
+            if (image != null) {
+                pic.push(image)
+            }
+        }
+        // if no images were founded in API response -> return a default image
+        if (!pic.length) {
+            var eventType = seatgeekEvent.type;
+            pic.push(Image[eventType] || Image.default);
+        }
+        // return an array of pic in case the event has a few performers and every perfermer has its own pic.
+        return pic;
+
+
     }
-})();
-
 });
